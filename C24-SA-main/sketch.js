755d5d2e150313876923726114ec97049e136ab8
Matheus Feresin Mancini <164958883+Omni-Mancini@users.noreply.github.com>
2024-06-06 12:32:39 -0300

    Add files via upload

---
+++ C24-SA-main/sketch.js
@@ -0,0 +1,85 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var btn2;
+var angle = 60;
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+   var ball_options = {
+    restitution: 0.95,
+    frictionAir:0.01
+  }
+   var ground_options ={
+     isStatic: true
+   };
+  btn2 = createImg('up.png');
+  btn2.position(350,30);
+  btn2.size(50,50);
+  btn2.mouseClicked(vForce);
+  
+  btn1 = createImg('right.png');
+  btn1.position(300,30);
+  btn1.size(50,50);
+  btn1.mouseClicked(vForce1);
+  
+
+  ground1 = Bodies.rectangle(100,200,400,20,ground_options);
+  World.add(world,ground1);
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+  
+  ground = Bodies.rectangle(100,400,650,20,ground_options);
+  World.add(world,ground); 
+  
+  ball1 = Bodies.circle(200,10,20,ball_options);
+  World.add(world,ball1);
+  
+  
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+
+  
+Matter.Body.rotate(ground1,angle);
+push();
+translate(ground1.position.x,ground1.position.y);
+rotate(angle);
+rect(0,0,20,400);
+pop();
+angle+=0.1;
+
+  
+  ellipse(ball.position.x,ball.position.y,20);
+  ellipse(ball1.position.x,ball1.position.y,20);
+  rect(ground.position.x,ground.position.y,650,20)  
+ // rect(ground1.position.x,ground1.position.y,400,20)  
+}
+
+function vForce()
+{
+  //  Matter.Body.applyForce(*nome do corpo*,{x:*inicial*,y:*inicial*},{x:0,y:-0.05});
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05});
+  Matter.Body.applyForce(ball1,{x:0,y:0},{x:0,y:-0.05});
+}
+
+function vForce1()
+{
+  //  Matter.Body.applyForce(*nome do corpo*,{x:*inicial*,y:*inicial*},{x:0,y:-0.05});
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:+0.05,y:0});
+  Matter.Body.applyForce(ball1,{x:0,y:0},{x:+0.05,y:0});
+}
